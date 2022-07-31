6577290757fc9c3292ce5eedde067e5bcb484e4d
JEBA <fawjiajeba@gmail.com>
2022-07-31 18:44:48 +0600

    reviews

--- src/Pages/Questions/Questions.js
+++ src/Pages/Questions/Questions.js
@@ -3,32 +3,54 @@ import './Questions.css';
 
 const Questions = () => {
       return (
-            <div className='qn-container'>
-                  <div style={{ width: '500px' }} tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                        <div class="collapse-title text-xl font-medium">
-                              Focus me to see content
+            <div>
+                  <h1 className='qn-title'>Questions and Answer</h1>
+                  <div className='qn-container'>
+
+                        <div style={{ width: '500px' }} tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                              <div class="collapse-title text-xl font-medium">
+                                    What is the tools of Mason?
+                              </div>
+                              <div class="collapse-content">
+                                    <p>Stonemason's Hammer. The traditional stonemason's hammer (or mash hammer) is a versatile two-sided hand tool that allows you to deliver two different types of strikes. ...
+                                          Tape Measure and Bubble Level. ...
+                                          Power Saw. ...
+                                          Wire Brush. ...
+                                          Mason's Trowel. ...
+                                          Jointers. ...
+                                          Straight Edge. ...
+                                          Masonry Square.</p>
+                              </div>
                         </div>
-                        <div class="collapse-content">
-                              <p>tabindex="0" attribute is necessary to make the div focusable</p>
+                        <div style={{ width: '500px' }} tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                              <div class="collapse-title text-xl font-medium">
+                                    What is the most important tool in masonry?
+
+                              </div>
+                              <div class="collapse-content">
+                                    <p>
+                                          Arguably the signature tool of masons, there are a few different ones that you need to be aware of. The main trowel is the V-trowel, also known as the square-notch trowel. This tool has two straight sides and another two notched sides. These are designed to dispense mortar around a flat surface at even rates.</p>
+                              </div>
                         </div>
-                  </div>
-                  <div style={{ width: '500px' }} tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                        <div class="collapse-title text-xl font-medium">
-                              Focus me to see content
-                        </div>
-                        <div class="collapse-content">
-                              <p>tabindex="0" attribute is necessary to make the div focusable</p>
-                        </div>
-                  </div>
-                  <div style={{ width: '500px' }} tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                        <div class="collapse-title text-xl font-medium">
-                              Focus me to see content
-                        </div>
-                        <div class="collapse-content">
-                              <p>tabindex="0" attribute is necessary to make the div focusable</p>
+                        <div style={{ width: '500px' }} tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                              <div class="collapse-title text-xl font-medium">
+                                    What are mortar tools?
+
+                              </div>
+                              <div class="collapse-content">
+                                    <p>Margin Trowel.
+                                          V- or Square-Notch Trowel.
+                                          Cold Chisel.
+                                          Brick Hammer or Mason's Hammer.
+                                          Wire Brush.
+                                          Masonry Materials Your Should Buy.
+                                          Veneer Mortar.
+                                          Rebar.</p>
+                              </div>
                         </div>
                   </div>
             </div>
+
       );
 };
 
--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReviewCard from './ReviewCard';
 import review from '../../images/review.png'
+import './Reviews.css';
 
 const Reviews = () => {
       return (
             <div>
-                  <h1>Reviews</h1>
+                  <h1 className='title'>Reviews</h1>
                   <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
                         <ReviewCard cardTitle="James Jeson" cardDescription="Enjoyed staying: location is great, not far from a subway station and only 15 minutes ride from all the sights. There are lots of restaurants and shops nearby. Cheerful and welcoming atmosphere. Thanks for the hospitality." img={review}></ReviewCard>
                         <ReviewCard cardTitle="Rimika Ghosh" cardDescription="Every hotel today should have an unspoken promise to provide a comfortable place to stay and a high level of customer service to each of its guests. If that promise is being met, let travelers know through review response. " img={review}></ReviewCard>
